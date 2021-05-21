e339e334f59ebad27093878cd5744b45656a321f
Nils Wieler <nils.wieler@gmail.com>
2021-05-21 10:33:34 +0200

    Update opus_flutter.podspec after upgrading flutter plugin

--- ios/Classes/SwiftOpusFlutterPlugin.swift
+++ ios/Classes/SwiftOpusFlutterPlugin.swift
@@ -3,12 +3,8 @@ import UIKit
 
 public class SwiftOpusFlutterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "opus_flutter", binaryMessenger: registrar.messenger())
-    let instance = SwiftOpusFlutterPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
+    
   }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+  
 }
